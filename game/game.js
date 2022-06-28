7afea631c8c86f9c734c78df2317552f8b68286a
Ske1l <hayley6236@gmail.com>
2022-06-28 15:46:11 -0400

    working collision detection

--- game/game.js
+++ game/game.js
@@ -10,5 +10,28 @@ function jump() {
     character.classList.add("animate")
     setTimeout(function () {
         character.classList.remove("animate");
-    }, 1000)
+    }, 1000);
 }
+
+fuction checkCollision(character, obstacle){
+    if (
+        character.x + character.width >= obstacle.x &&
+        character.x <= obstacle.x + obstacle.width &&
+        character.y + character.height >= obstacle.y &&
+        character.y <= obstacle.y + obstacle.height
+    ) {
+        obstacle.style.animation = "none"
+        alert("you lose")
+    };
+};
+
+// let checkCollision = setInterval(function () {
+//     let characterTop =
+//         parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+//     let objectLeft =
+//         parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+//     if (objectLeft < 30 && objectLeft > 0 && characterTop >= 410) {
+//         obstacle.style.animation = "none"
+//         alert("u lose")
+//     }
+// }, 10);
