ee267c98d588296eef2334d365fe10735dcfbd44
Ske1l <hayley6236@gmail.com>
2022-06-28 13:24:13 -0400

    added jump function and onclick event listener

--- game/game.js
+++ game/game.js
@@ -1,4 +1,11 @@
 // define objects
-
+let character = document.getElementById("character");
+let obstacle = document.getElementById("obstacle")
 
 // define function to add jump key frame to character with addeventlistener
+
+document.addEventListener("click", jump)
+
+function jump() {
+    character.classList.add("animate")
+}
